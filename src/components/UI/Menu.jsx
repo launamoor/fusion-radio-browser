89104ca0f3c964bb2de9315fb201f42cabd9a849
launamoor <116520556+launamoor@users.noreply.github.com>
2023-08-09 23:01:31 +0100

    Update Menu.jsx

--- src/components/UI/Menu.jsx
+++ src/components/UI/Menu.jsx
@@ -7,28 +7,28 @@ const Menu = function () {
     <nav className="flex flex-col items-stretch gap-4 mb-12 self-stretch">
       <MenuItem
         to="/"
-        className="flex items-start justify-start gap-4 menu-item text-xl font-semibold"
+        className="flex items-start justify-center xs:justify-start gap-4 menu-item text-xl font-semibold"
       >
         <FaHome className="text-3xl text-current" />
         <span>Home</span>
       </MenuItem>
       <MenuItem
         to="/search"
-        className="flex items-start justify-start gap-4 menu-item text-xl font-semibold"
+        className="flex items-start justify-center xs:justify-start gap-4 menu-item text-xl font-semibold"
       >
         <FaSearch className="text-3xl text-current" />
         <span>Search</span>
       </MenuItem>
       <MenuItem
         to="/saved"
-        className="flex items-start justify-start gap-4 menu-item text-xl font-semibold"
+        className="flex items-start justify-center xs:justifgy-start gap-4 menu-item text-xl font-semibold"
       >
         <BsHeadphones className="text-3xl text-current" />
         <span>Saved Stations</span>
       </MenuItem>
       <MenuItem
         to="/about"
-        className="flex items-start justify-start gap-4 menu-item text-xl font-semibold"
+        className="flex items-start justify-center xs:justify-start gap-4 menu-item text-xl font-semibold"
       >
         <FaInfoCircle className="text-3xl text-current" />
         <span>About</span>
